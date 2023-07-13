251212513af97b482bf760af5cacdbd62a470a4b
Leo Tumwattana <leo.tumwattana@gmail.com>
2023-07-13 13:00:17 +0800

    Initial Commit

---
+++ MenuOptionalPrimaryAction/MenuOptionalPrimaryActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuOptionalPrimaryActionApp.swift
+//  MenuOptionalPrimaryAction
+//
+//  Created by Leo Tumwattana on 13/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuOptionalPrimaryActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
